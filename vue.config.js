cb315306b80eb0c36d10b5fb868c4f62fd44178a
coderjhk <wanted0301@gmail.com>
2021-10-13 18:47:14 +0900

    feat: add vuetify-admin

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: "false",
+      enableBridge: undefined,
+    },
+  },
   transpileDependencies: ["vuetify"],
 };
