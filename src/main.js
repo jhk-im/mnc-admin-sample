8d2c6b56997d8ee104fc168525382130eac24deb
coderjhk <wanted0301@gmail.com>
2021-10-18 18:10:29 +0900

    feat: dashboard sample 추가 - 2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
-//import i18n from "./i18n";
+import i18n from "./i18n";
 import { sync } from "vuex-router-sync";
 import "./plugins";
 import "./sass/overrides.sass";
@@ -17,5 +17,6 @@ new Vue({
   router,
   vuetify,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
