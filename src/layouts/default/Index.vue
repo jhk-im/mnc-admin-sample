29f064cabdb6751da946fbc04c75351b3f8cd97f
coderjhk <wanted0301@gmail.com>
2021-10-18 18:10:48 +0900

    feat: dashboard sample 추가 - 3

---
+++ src/layouts/default/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <default-bar />
+
+    <default-drawer />
+
+    <default-view />
+
+    <default-footer />
+
+    <default-settings />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+
+  components: {
+    DefaultBar: () =>
+      import(
+        /* webpackChunkName: "default-app-bar" */
+        "./AppBar"
+      ),
+    DefaultDrawer: () =>
+      import(
+        /* webpackChunkName: "default-drawer" */
+        "./Drawer"
+      ),
+    DefaultFooter: () =>
+      import(
+        /* webpackChunkName: "default-footer" */
+        "./Footer"
+      ),
+    DefaultView: () =>
+      import(
+        /* webpackChunkName: "default-view" */
+        "./View"
+      ),
+  },
+};
+</script>
